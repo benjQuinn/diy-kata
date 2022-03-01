24fe4b3a5163b771539ae751156f71b2d4e5ca12
Ben Quinn <benjamesquinn@gmail.com>
2022-03-01 21:43:41 +0000

    Completed numberToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,14 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  return number
+    .toString()
+    .split("")
+    .reverse()
+    .map(Number);
+};
+
+// 1) Convert number to string so we can reverse()
+// 2) Split the string into an Array
+// 3) Reverse the Array
+// 4) Create new array converting every element to a Number
 
 module.exports = numberToReversedDigits;
