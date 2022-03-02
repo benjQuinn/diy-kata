7cd1216a80c23e0cc6dc168b9e9bcac249501fcb
Ben Quinn <benjamesquinn@gmail.com>
2022-03-02 17:44:58 +0000

    Completed reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const time = distance / speed;
+  return `I should be there in ${Math.round(time * 2) / 2} hours`;
+};
 
 module.exports = reachDestination;
