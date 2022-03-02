7b49225e356293d5d45f744ea4e46d5bc345e1a9
Ben Quinn <benjamesquinn@gmail.com>
2022-03-02 18:14:05 +0000

    joinNames WIP

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,10 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  namesObj.reduce((acc, person, index) => {
+    if (index < namesObj.length - 1) {
+      return `${acc + person.name}, `;
+    }
+    return `${acc}& ${person.name}`;
+  }, "");
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,16 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const array1 = [{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }];
+    const array2 = [
+      { name: "Bart" },
+      { name: "Lisa" },
+      { name: "Maggie" },
+      { name: "Homer" }
+    ];
+
+    expect(joinNames(array1).toEqual("Bart, Lisa & Maggie"));
+    expect(joinNames(array2).toEqual("Bart, Lisa, Maggie & Homer"));
+  });
 });
