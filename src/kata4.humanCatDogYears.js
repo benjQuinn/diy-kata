70b37aed084e71348f7dd01ebca69f022dcecb18
Ben Quinn <benjamesquinn@gmail.com>
2022-03-02 17:24:39 +0000

    Completed humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,26 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  let cat = 0;
+  let dog = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (i === 1) {
+      cat += 15;
+      dog += 15;
+    } else if (i === 2) {
+      cat += 9;
+      dog += 9;
+    } else {
+      cat += 4;
+      dog += 5;
+    }
+  }
+  return [number, cat, dog];
+};
 
 module.exports = humanCatDogYears;
+
+// Goes through the loop in human years and adds to accumulator every time code block is executed
+
+// e.g if number passed in is 3, first codeblock executes (0 + 15) and then second  code block executes (15 + 9) and then third and final code block executes (14 + 4)
+
+//     returns [3, 28, 29]
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require("../src");
 
 // Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns array of human, cat and dog years when passed human years", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  });
+});
+
+// toEqual as we want to return an Array
+// Only use toBe for primitive data types - strings, booleans, numbers
